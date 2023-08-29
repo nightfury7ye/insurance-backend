be81c6cd3cdeeca9576bb63d855d03aae48ef17e
Pavan <pavanvemula7@gmail.com>
2023-08-29 20:40:27 +0530

    shubham test2

--- src/main/java/com/techlabs/insurance/service/PolicyServiceImpl.java
+++ src/main/java/com/techlabs/insurance/service/PolicyServiceImpl.java
@@ -150,7 +150,7 @@ public class PolicyServiceImpl implements PolicyService{
 
 	@Override
 	public Payment payInstallment(Payment payment ,int paymentid) {
-		System.out.println("pay installments");
+		System.out.println("pay installment");
 		Payment dbpayment = paymentRepo.findById(paymentid).get();
 		dbpayment.setPaymenttype(payment.getPaymenttype());
 		Optional<Payment_status> status= paymentStatusRepo.findById(1);
