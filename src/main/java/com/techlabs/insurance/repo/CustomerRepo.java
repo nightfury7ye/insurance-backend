19ae39a211eeb66ed3b0ed5d9bb95c22ba119cc6
nightfury7ye <73484604+nightfury7ye@users.noreply.github.com>
2023-09-02 10:00:29 +0530

    Update CustomerRepo.java2

--- src/main/java/com/techlabs/insurance/repo/CustomerRepo.java
+++ src/main/java/com/techlabs/insurance/repo/CustomerRepo.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.techlabs.insurance.entities.Customer;
 
 public interface CustomerRepo extends JpaRepository<Customer, Integer>{
-    Page<Customer> findByUserstatusStatusid(int statusid, Pageable pageable);
-Customer findByUserUsername(String username);
+
+  Page<Customer> findByUserstatusStatusid(int statusid, Pageable pageable);
+
+  Customer findByUserUsername(String username);
 }
